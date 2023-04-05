56d63ad9c65e8e582e6bb55f333f8caeb3fb6ef9
David C <dchung20315@gmail.com>
2023-04-05 16:30:45 -0700

    added a load function to pull from local storage

--- script.js
+++ script.js
@@ -1,27 +1,42 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+//Outputs the current day to the html//
 var today = dayjs();
 var dayWeek = today.format("[Today is] dddd");
 $("#currentDay").text(dayWeek);
 
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+
+//reads each hour block and adds the corresponding class to that hour block//
+function setTimePeriod() {
+
+  var presentTime = parseInt(today.format("H"));
+
+  for (i=9; i< 18; i++) {
+    var timeBlock = document.getElementById("hour-" + i);
+
+    if (i < presentTime) {
+      timeBlock.classList.add("past");
+    }
+    else if (i > presentTime) {
+      timeBlock.classList.add("future");
+    }
+    else {
+      timeBlock.classList.add("present");
+    }
+  }
+
+}
+
+setTimePeriod();
+
+
+//adds an eventlistener to the save button to store in local storage//
+var saveButton = document.querySelectorAll(".saveBtn");
+
+
+window.addEventListener("load", function() {
+  for (i=9; i < 18; i++) {
+    var savedData = localStorage.getItem("hour-" + i);
+    if (savedData) {
+      inputText.value = savedData;
+    }
+}
 });
